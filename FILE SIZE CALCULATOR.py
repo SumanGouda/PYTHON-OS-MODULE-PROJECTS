28ca74fe232d0750c84190a8377c4381f3a6bacd
Suman Gouda <mr.sumangouda@gmail.com>
2025-05-13 22:15:54 +0530

    Update FILE SIZE CALCULATOR.py

--- FILE SIZE CALCULATOR.py
+++ FILE SIZE CALCULATOR.py
@@ -3,13 +3,21 @@ import tkinter
 from tkinter import filedialog
 
 folder = filedialog.askdirectory(title="Select your folder")
+    #Opens the file manager window that allows the user to select folder
+
+if not folder:
+    print("No folder selected.")
+    exit()
+
 for file in os.listdir(folder):
+        #Makes a list of all the files in the folder
     file_path = os.path.join(folder,file)
     if not os.path.isfile(file_path):
+        #Skip the subfolder inside the folder 
         continue
-    size = os.path.getsize(file_path)
-    size_kb = size / 1024
+    
+    size_kb = os.path.getsize(file_path) / 1024
     rounded_kb = round(size_kb,2)
-    print(file," kb" ,end=" ")
-    print(size_kb," kb")
+    print(file," KB" ,end=" ")
+    print(rounded_kb," KB")
         
